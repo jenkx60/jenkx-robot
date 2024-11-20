20109a85ae760634e22519f6403964fd3fad8788
jenkx60 <jenkinsuwagbai18@gmail.com>
2024-11-20 10:07:53 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <p>Welcome to Git & GitHub</p>
+    </div>
+  );
+}
+
+export default App;
